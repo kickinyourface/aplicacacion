7508d78d0fe144c367f5f73867c68d6d22372288
Fire-Frog <fabian.barrera.q@gmail.com>
2023-12-13 17:16:35 -0300

    [FEAT] Fixed Fetch query
    Modified fetch URL structure.
    Added check to fetch response (status).
    Added message to user when fetch fails.
    Modified .gitignore to allow .env files.
    Added .env file (to change default dev-server port -> 3000)
    Updated README.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,54 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [lastFetchStatus, setLastFetchStatus] = useState(true);
   const [query, setQuery] = useState('');
   const [books, setBooks] = useState([]);
 
   const searchBooks = async () => {
     try {
-      const response = await fetch(`/api/search_books?q=${query}`);
-      const data = await response.json();
-      setBooks(data.items || []);
+      const urlBackAPI = "http://localhost:3001/api/search_books"
+      const queryParams = new URLSearchParams({ q: query }).toString();
+      const response = await fetch(`${urlBackAPI}?${queryParams}`);
+
+      // check if the response is "OK"
+      if (response.status === 200) {
+        setLastFetchStatus(true);
+        const data = await response.json();
+        setBooks(data.items || []);
+        return;
+      }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
+
+    // if response was not successful
+    setBooks([]);
+    setLastFetchStatus(false);
   };
 
   return (
     <div>
       <h1>Google Books Search</h1>
       <div>
-        <input 
-          type="text" 
-          value={query} 
-          onChange={(e) => setQuery(e.target.value)} 
-          placeholder="Enter search query" 
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter search query"
         />
         <button onClick={searchBooks}>Search</button>
+      </div>
+      <div>
+        <br />
+        <span
+          style={{
+            color: "red",
+            visibility: lastFetchStatus ? "hidden" : "inherit"
+          }}
+        >
+          Error while getting the fetched response
+        </span>
       </div>
       <div>
         {books.map((book) => (
